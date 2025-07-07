0d85e660aeb641ed5af3cff0c2e5716d28525451
nielassis <nielvitorba@gmail.com>
2025-07-07 15:49:52 -0300

    feat(schema): add updateTeacherSchema with support for optional fields and password change

--- src/objects/teacher.schemas.ts
+++ src/objects/teacher.schemas.ts
@@ -6,3 +6,10 @@ export const createTeacherSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
 });
+
+export const updateTeacherSchema = z.object({
+  name: z.string().min(3).optional(),
+  email: z.string().email().optional(),
+  oldPassword: z.string().min(6).optional(),
+  newPassword: z.string().min(6).optional(),
+});
