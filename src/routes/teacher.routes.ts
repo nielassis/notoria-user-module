8bded0c3f0dbeec426e1653438dde59d76d34953
nielassis <nielvitorba@gmail.com>
2025-07-07 15:50:15 -0300

    feat(routes): add protected routes for teacher profile and update with JWT verification

--- src/routes/teacher.routes.ts
+++ src/routes/teacher.routes.ts
@@ -1,10 +1,27 @@
 import { FastifyInstance } from "fastify";
-import { createTeacher, teacherLogin } from "../controllers/teacher.controller";
+import {
+  createTeacher,
+  teacherLogin,
+  teacherProfile,
+  teacherUpdate,
+} from "../controllers/teacher.controller";
+import { verifyJWT } from "../middlewares/verifyJWT";
 
 export async function teacherRoutes(server: FastifyInstance) {
   server.post("/create", createTeacher);
 
   server.post("/login", teacherLogin);
+
+  /* A partir daqui, todas as rotas ter o o hook de verificao de JWT aplicado, 
+  garantindo que apenas usuarios autenticados possam acessar as rotas protegidas.
+  */
+  server.register(async (privateRoutes) => {
+    privateRoutes.addHook("onRequest", verifyJWT);
+
+    privateRoutes.get("/profile", teacherProfile);
+
+    privateRoutes.put("/profile", teacherUpdate);
+  });
 }
 
 export default teacherRoutes;
