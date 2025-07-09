a8f15c97425ecddf0b7e7875c2e3e5ebd64f5680
nielassis <nielvitorba@gmail.com>
2025-07-09 15:58:54 -0300

    feat(routes): implement classroom and class-student routes

---
+++ src/routes/classrooms.routes.ts
@@ -0,0 +1,56 @@
+import { FastifyInstance } from "fastify";
+import {
+  createClassroom,
+  deleteClassroom,
+  getAllClassrooms,
+  getClassroomById,
+  updateClassroom,
+} from "../controllers/classroom.controller";
+import { verifyJWT } from "../middlewares/verifyJWT";
+import {
+  getStudentByIdInClassroom,
+  getStudentsInClassroom,
+  insertStudentInClassroom,
+  removeStudentFromClassroom,
+  updateStudentScoreInClassroom,
+} from "../controllers/classes.controller";
+
+export default async function classroomRoutes(server: FastifyInstance) {
+  server.register(async (privateRoutes) => {
+    privateRoutes.addHook("onRequest", verifyJWT);
+
+    privateRoutes.post("/", createClassroom);
+
+    privateRoutes.get("/", getAllClassrooms);
+
+    privateRoutes.get("/:classroomId", getClassroomById);
+
+    privateRoutes.put("/:classroomId", updateClassroom);
+
+    privateRoutes.delete("/:classroomId", deleteClassroom);
+
+    // ações professor e ambiente sala de aula -> estudantes
+
+    privateRoutes.post(
+      "/classes/:classroomId/:studentId",
+      insertStudentInClassroom
+    );
+
+    privateRoutes.delete(
+      "/classes/:classroomId/:studentId",
+      removeStudentFromClassroom
+    );
+
+    privateRoutes.put(
+      "/classes/:classroomId/:studentId",
+      updateStudentScoreInClassroom
+    );
+
+    privateRoutes.get("/classes/:classroomId", getStudentsInClassroom);
+
+    privateRoutes.get(
+      "/classes/:classroomId/:studentId",
+      getStudentByIdInClassroom
+    );
+  });
+}
